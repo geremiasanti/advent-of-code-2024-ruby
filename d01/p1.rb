d36b6b4a94bd5b43749f74219001a2f01bd822bb
geremia <germysettesette@gmail.com>
2025-01-25 17:32:56 +0100

    d01p1

---
+++ d01/p1.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+
+def main
+  if ARGV.empty?
+    puts 'Missing input file'
+    exit(1)
+  end
+
+  file_path = ARGV[0]
+  pp solve(File.new(file_path, 'r'))
+end
+
+def solve(input_file)
+  left_line, right_line = input_file.readlines.map { |l| l.split.map(&:to_i) }
+                                    .flatten
+                                    .partition.with_index { |_, i| i.even? }
+                                    .map(&:sort)
+  left_line.zip(right_line).reduce(0) { |distances_sum, (l, r)| distances_sum + (l - r).abs }
+end
+
+main
