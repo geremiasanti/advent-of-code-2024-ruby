e9e568251bd52a65971d1753a62fb4ca1904eb83
geremia <germysettesette@gmail.com>
2025-01-25 21:33:53 +0100

    d02p1

---
+++ d02/p1.rb
@@ -0,0 +1,31 @@
+# frozen_string_literal: true
+
+def main
+  if ARGV.empty?
+    puts 'Missing input file'
+    exit(1)
+  end
+
+  file_path = ARGV[0]
+  pp solve(File.new(file_path, 'r'))
+end
+
+def solve(input_file)
+  input_file.readlines.map { |l| l.split.map(&:to_i) }
+    # convert report value to report increments
+    .map { |r| r.each_cons(2).map { |l1, l2| l2 - l1 } }
+    # count safe reports
+    .count do |r|
+      next false if r.any? { |l| l.abs > 3 }
+
+      increments = r.count(&:positive?)
+      next false if !increments.zero? && increments < r.size
+
+      decrements = r.count(&:negative?)
+      next false if !decrements.zero? && decrements < r.size
+
+      true
+    end
+end
+
+main
