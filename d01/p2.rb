4e5629b1a56b606ca1ff4a1c44d6dd8192fc00ac
geremia <germysettesette@gmail.com>
2025-01-25 20:14:30 +0100

    d01p2

--- d01/p2.rb
+++ d01/p2.rb
@@ -14,9 +14,8 @@ def solve(input_file)
   left_line, right_line = input_file.readlines.map { |l| l.split.map(&:to_i) }
                                     .flatten
                                     .partition.with_index { |_, i| i.even? }
-                                    .map(&:sort)
 
-  left_line.zip(right_line).reduce(0) { |distances_sum, (l, r)| distances_sum + (l - r).abs }
+  left_line.map { |id| id * right_line.count(id) }.reduce(:+)
 end
 
 main
