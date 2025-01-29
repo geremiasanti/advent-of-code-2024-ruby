9b3c4b2312a5ca24dccb7b78d7d99b02c577b744
Geremia Santi <geremiasanti.dev@gmail.com>
2025-01-29 14:07:23 +0100

    d03p2 wip

---
+++ d03/p2.rb
@@ -0,0 +1,35 @@
+# frozen_string_literal: true
+
+def main
+  if ARGV.empty?
+    puts 'Missing input file'
+    exit(1)
+  end
+
+  file_path = ARGV[0]
+  pp solve(File.new(file_path, 'r'))
+end
+
+def solve(input_file)
+  puts txt_content = input_file.read
+
+  enabled = true
+  txt_content
+    .scan(/do\(\)|don't\(\)|mul\([0-9]{1,3},[0-9]{1,3}\)/)
+    .reduce(0) { |acc, instruction| 
+      if instruction == 'do()' 
+        enabled = true 
+        next acc
+      end
+      if instruction == 'don\'t()'
+        enabled = false 
+        next acc
+      end
+      next if !enabled
+
+      p1, p2 = instruction.scan(/[0-9]{1,3}/).map(&:to_i)
+      acc + p1 * p2
+    }
+end
+
+main
