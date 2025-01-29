d601ce472e2f88c398d40e7ad4c8e64006f44df3
geremia <germysettesette@gmail.com>
2025-01-30 00:52:21 +0100

    d04p1 wip

---
+++ d04/p1.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+def main
+  if ARGV.empty?
+    puts 'Missing input file'
+    exit(1)
+  end
+
+  file_path = ARGV[0]
+  pp solve(File.new(file_path, 'r'))
+end
+
+def solve(input_file)
+  puts input = input_file.read
+end
+
+main
