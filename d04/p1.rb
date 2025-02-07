9974de32526065e3d9312b3a48e8b4ba437c6277
Geremia Santi <geremiasanti.dev@gmail.com>
2025-02-07 19:11:52 +0100

    d04p1

--- d04/p1.rb
+++ d04/p1.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 def main
   if ARGV.empty?
     puts 'Missing input file'
@@ -11,8 +9,31 @@ def main
 end
 
 def solve(input_file)
-  puts input = input_file.read
-  input.split("\n")
+  input = input_file.read
+
+  rows, cols, anti_diags = [], [], []
+  main_diags = {}
+  input.each_line.with_index do |row, i_row|
+    row.chomp.each_char.with_index do |char, i_col|
+      rows[i_row] ||= ''
+      rows[i_row] << char
+
+      cols[i_col] ||= ''
+      cols[i_col] << char
+
+      i_main_diags = i_row - i_col
+      main_diags[i_main_diags] ||= ''
+      main_diags[i_main_diags] << char
+
+      i_anti_diags = i_row + i_col
+      anti_diags[i_anti_diags] ||= ''
+      anti_diags[i_anti_diags] << char
+    end
+  end
+
+  (rows + cols + main_diags.values + anti_diags).reduce(0) do |acc, s|
+    acc + s.reverse.scan('XMAS').count + s.scan('XMAS').count
+  end
 end
 
 main
